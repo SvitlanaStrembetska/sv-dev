6ab754ff6c377e3aaee50d7bcf8328c1d5a80636
Svitlana Dolinovska <sdolinovska@gmail.com>
2017-12-24 22:37:27 +0200

    Add person and filter ViewModels

---
+++ Svbase/Svbase.Core/Models/FilterSearchModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Svbase.Core.Data.Entities;
+using Svbase.Core.Enums;
+
+namespace Svbase.Core.Models
+{
+    public class FilterSearchModel
+    {
+        public ICollection<int> StreatIds { get; set; }
+        public ICollection<int> DistrictIds { get; set; }
+        public DistrictType DistrictType { get; set; }
+        public ICollection<int> FlatIds { get; set; }
+        public ICollection<int> ApartmentIds { get; set; }
+        public ICollection<int> BeneficiaryIds { get; set; }
+    }
+}
--- Svbase/Svbase.Core/Models/PersonViewModel.cs
+++ Svbase/Svbase.Core/Models/PersonViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace Svbase.Core.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Svbase.Core.Data.Entities;
+
+namespace Svbase.Core.Models
 {
-    public class PersonViewModel : BaseViewModel
+    public class PersonViewModel 
     {
+        public int Id { get; set; }
+        [Display(Name = "FirthName")]
+        public string FirthName { get; set; }
+        [Display(Name = "MiddleName")]
+        public string MiddleName { get; set; }
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+        [Phone]
+        public string FirthtMobilePhone { get; set; }
+        [Phone]
+        public string SecondMobilePhone { get; set; }
+        [Phone]
+        public string HomePhone { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public DateTime DateBirth { get; set; }
+        public string Position { get; set; }
+        public string PartionType { get; set; }
+        public bool Gender { get; set; }
+
+        public int BeneficiaryId { get; set; }
+        public int DistrictId { get; set; }
+        public int FlatId { get; set; }
     }
 }
